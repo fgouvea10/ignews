6d6035ad7c4742f9594329e83d35ad443b79a555
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-16 01:19:06 -0300

    redirecting to stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { signIn, useSession } from 'next-auth/react';
 
 import { Container } from './styles';
+import { api } from 'services/api';
+import { getStripeJs } from 'services/stripe-js';
+import toast from 'react-hot-toast';
 
 type SubscribeButtonProps = {
   priceId: string;
@@ -10,10 +13,23 @@ type SubscribeButtonProps = {
 function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
 
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
     if (!session) {
       signIn('github');
       return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+
+    } catch (err) {
+      toast.error(err.message);
     }
   }
 
