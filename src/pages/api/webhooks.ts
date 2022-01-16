42edfd69a720acc6682b5b8bee81305bd2dabdbb
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-16 13:33:43 -0300

    listening more events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -20,7 +20,11 @@ export const config = {
   },
 };
 
-const relavantEvents = new Set(['checkout.session.completed']);
+const relavantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -44,14 +48,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relavantEvents.has(type)) {
       try {
         switch (type) {
-          case 'checkout.session.completed':
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
 
-          const checkoutSession = event.data.object as Stripe.Checkout.Session;
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
+          case 'checkout.session.completed':
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
               checkoutSession.customer.toString(),
-            )
+              true
+            );
 
             break;
           default:
